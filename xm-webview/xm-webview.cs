2bd9b5ace4cded2255efc9d8d4fe3993ce8b252f
Manh Vu <manh.vv.htth@gmail.com>
2015-06-06 01:24:47 +0700

    Initial check-in of module xm-webview

---
+++ xm-webview/xm-webview.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace xmwebview
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			WebView webView = new WebView {
+				Source = new UrlWebViewSource {
+					Url = "http://192.168.251.1:5000/test"
+				},
+				HorizontalOptions = LayoutOptions.Start,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				BackgroundColor = Color.Blue
+			};
+
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.FillAndExpand,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!",
+							TextColor = Color.White,
+							HeightRequest = 30,
+							BackgroundColor = Color.Pink,
+						},
+						webView
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
